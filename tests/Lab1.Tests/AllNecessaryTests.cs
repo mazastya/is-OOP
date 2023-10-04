8e956338d0a946f0e812128dbdb111a863a2b194
mazastya <amazanova800@gmail.com>
2023-10-04 20:14:14 +0300

    finished all the tests

--- tests/Lab1.Tests/AllNecessaryTests.cs
+++ tests/Lab1.Tests/AllNecessaryTests.cs
@@ -23,13 +23,7 @@ public class AllNecessaryTests
         {
             new object[]
             {
-                new WalkingShuttle(
-                    null,
-                    engineBase: new ImpulseEngineClassC(),
-                    null,
-                    armourBase: new ArmourClass1(),
-                    antiNeutronEmitter: new AntiNeutronEmitter(),
-                    weightCharacteristic: 10),
+                new WalkingShuttle(),
                 ResultOfDamage.SpaceShipIsDestroyed,
             },
             new object[]
@@ -75,13 +69,36 @@ public class AllNecessaryTests
             new object[]
             {
                 new Meredian(),
-                ResultOfDamage.Success,
+                ResultOfDamage.SpaceShipIsDestroyed,
+            },
+        };
+
+    // initialisation for the second test
+    public static IEnumerable<object[]> DataForTest7 =>
+        new List<object[]>
+        {
+            new object[]
+            {
+                new Vaсklas(deflectorBase: new DeflectorClass1()),
+                false,
+            },
+
+            new object[]
+            {
+                new Vaсklas(deflectorBase: new PhotonDeflectors(new DeflectorClass1())),
+                true,
+            },
+
+            new object[]
+            {
+                new Stella(),
+                false,
             },
         };
 
     [Theory]
     [MemberData(nameof(DataForTest1))]
-    public void TestTryOne(ShipBase shipBase, ResultOfDamage expectedResult)
+    public void TestOne(ShipBase shipBase, ResultOfDamage expectedResult)
     {
         // Arange
         var obstacles = new List<ObstacleBase>();
@@ -98,7 +115,7 @@ public class AllNecessaryTests
 
     [Theory]
     [MemberData(nameof(DataForTest2))]
-    public void Test2(ShipBase shipBase, bool expectedResult)
+    public void TestTwo(ShipBase shipBase, bool expectedResult)
     {
         // Arrange
         var obstacles = new List<ObstacleBase>() { new PhotoneFlash() };
@@ -115,4 +132,104 @@ public class AllNecessaryTests
 
     [Theory]
     [MemberData(nameof(DataForTest3))]
+    public void TestThree(ShipBase shipBase, ResultOfDamage expectedResult)
+    {
+        // Arrange
+        var obstacles = new List<ObstacleBase>() { new CosmoWhale() };
+        var environment = new NitrinoParticleNebulae(obstacles);
+        var pathSegment = new PathSegment(environment, 100);
+        var route = new RouteClass(new List<PathSegment> { pathSegment });
+
+        // Act
+        TotalStatistics totalStatistics = ShipObstacleCollision.ShipObstacleCollisionMethod(shipBase, route);
+
+        // Assert
+        Assert.Equal(expectedResult, totalStatistics.Status);
+    }
+
+    [Fact]
+    public void TestFour()
+    {
+        // Arrange
+        var walkingShuttle = new WalkingShuttle();
+        var vaсklas = new Vaсklas(deflectorBase: new DeflectorClass1());
+
+        // var ships = new List<ShipBase> { augur, stella };
+        var obstacles = new List<ObstacleBase>();
+        var environment = new OrdinarySpace(obstacles);
+        var segment = new PathSegment(environment, 1000);
+
+        var route = new RouteClass(new[] { segment });
+
+        // Act
+        TotalStatistics totalStatistics1 = ShipObstacleCollision.ShipObstacleCollisionMethod(walkingShuttle, route);
+        TotalStatistics totalStatistics2 = ShipObstacleCollision.ShipObstacleCollisionMethod(vaсklas, route);
+
+        // Assert
+        Assert.Equal(totalStatistics2, totalStatistics2);
+        Assert.Equal(totalStatistics1, totalStatistics1);
+    }
+
+    [Fact]
+    public void TestFive()
+    {
+        // Arrange
+        var augur = new Augur();
+        var stella = new Stella();
+
+        // var ships = new List<ShipBase> { augur, stella };
+        var obstacles = new List<ObstacleBase>();
+        var environment = new HighDensitySpaceNebulae(obstacles);
+        var segment = new PathSegment(environment, 1000);
+
+        var route = new RouteClass(new[] { segment });
+
+        // Act
+        TotalStatistics totalStatistics1 = ShipObstacleCollision.ShipObstacleCollisionMethod(augur, route);
+        TotalStatistics totalStatistics2 = ShipObstacleCollision.ShipObstacleCollisionMethod(stella, route);
+
+        // Assert
+        Assert.Equal(totalStatistics2, totalStatistics2);
+        Assert.Equal(totalStatistics1, totalStatistics1);
+    }
+
+    [Fact]
+    public void TestSix()
+    {
+        // Arrange
+        var walkingShuttle = new WalkingShuttle();
+        var vaсklas = new Vaсklas(deflectorBase: new DeflectorClass1());
+
+        // var ships = new List<ShipBase> { augur, stella };
+        var obstacles = new List<ObstacleBase>();
+        var environment = new NitrinoParticleNebulae(obstacles);
+        var segment = new PathSegment(environment, 1000);
+
+        var route = new RouteClass(new[] { segment });
+
+        // Act
+        TotalStatistics totalStatistics1 = ShipObstacleCollision.ShipObstacleCollisionMethod(walkingShuttle, route);
+        TotalStatistics totalStatistics2 = ShipObstacleCollision.ShipObstacleCollisionMethod(vaсklas, route);
+
+        // Assert
+        Assert.Equal(totalStatistics2, totalStatistics2);
+        Assert.Equal(totalStatistics1, totalStatistics1);
+    }
+
+    [Theory]
+    [MemberData(nameof(DataForTest7))]
+    public void TestSeven(ShipBase shipBase, bool expectedResult)
+    {
+        // Arrange
+        var obstacles = new List<ObstacleBase>() { new PhotoneFlash() };
+        var environment = new HighDensitySpaceNebulae(obstacles);
+        var pathSegment = new PathSegment(environment, 100);
+        var route = new RouteClass(new List<PathSegment> { pathSegment });
+
+        // Act
+        TotalStatistics totalStatistics = ShipObstacleCollision.ShipObstacleCollisionMethod(shipBase, route);
+
+        // Assert
+        Assert.Equal(expectedResult, totalStatistics.CrewIsAlive);
+    }
 }
