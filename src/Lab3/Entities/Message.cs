dafe8df142d5a0e836b686d6347a1eaf9e06596a
mazastya <amazanova800@gmail.com>
2023-10-28 22:43:09 +0300

    start work and create folders

---
+++ src/Lab3/Entities/Message.cs
@@ -0,0 +1,18 @@
+﻿namespace Itmo.ObjectOrientedProgramming.Lab3.Entities;
+
+public class Message
+{
+    public Message(
+        string title,
+        string bodyText,
+        string levelOfImportant)
+    {
+        Title = title;
+        BodyText = bodyText;
+        LevelOfImportant = levelOfImportant;
+    }
+
+    public string Title { get; set; }
+    public string BodyText { get; set; }
+    private string LevelOfImportant { get; set; }
+}
