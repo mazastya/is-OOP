7dfd480a93e765bc48c2b0d43f717386fbf2df6b
mazastya <amazanova800@gmail.com>
2023-11-17 22:21:49 +0300

    test exception

---
+++ src/Services/ChainOfResponsibilityForFlags/RenameFileFlag.cs
@@ -0,0 +1,18 @@
+﻿using Itmo.ObjectOrientedProgramming.Lab4.Services.IterationsWithFiles;
+
+namespace Itmo.ObjectOrientedProgramming.Lab4.Services.ChainOfResponsibilityForFlags;
+
+public class RenameFileFlag : HandlerBase
+{
+    protected override bool CanHandle(string[] request)
+    {
+        return request[0] == "file" && request[1] == "rename";
+    }
+
+    protected override string Handle(string[] request)
+    {
+        var renameFile = new RenameFile(request[2], request[3]);
+        renameFile.Execute(request[2]);
+        return "rename complite";
+    }
+}
---
+++ src/Services/IterationsWithFiles/MoveFile.cs
@@ -0,0 +1,20 @@
+﻿using Itmo.ObjectOrientedProgramming.Lab4.Entities;
+using File = System.IO.File;
+
+namespace Itmo.ObjectOrientedProgramming.Lab4.Services.IterationsWithFiles;
+
+public class MoveFile : Command
+{
+    public MoveFile(string pathFile, string newPathFile)
+        : base(pathFile)
+    {
+        NewPathFile = newPathFile;
+    }
+
+    private string NewPathFile { get; set; }
+
+    public override void Execute(string pathFile)
+    {
+        File.Move(pathFile, NewPathFile, true);
+    }
+}
