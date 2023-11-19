5d74e507db9c328e8b989edf0c6fefbfd78d2db6
mazastya <amazanova800@gmail.com>
2023-11-19 16:05:31 +0300

    add enter point

---
+++ src/Lab4/Programm.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Itmo.ObjectOrientedProgramming.Lab4;
+
+public static class Programm
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello World!");
+}
+}
